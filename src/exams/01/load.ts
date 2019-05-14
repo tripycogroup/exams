5edc90302e7d01e839399b01ce48f51916f25eb0
Tri Pham Minh <tri.pham@pycogroup.com>
2019-05-14 17:08:39 +0700

    docs: add comments about complexity

--- src/exams/01/load.ts
+++ src/exams/01/load.ts
@@ -8,6 +8,8 @@ const parse = (text: string): DICT =>
     split(';')
   )(text);
 
+// Complexity: O(n x m), which is equal to the complexity to traverse a 2d array
+// where n is size of array & m is size of dictionary
 export default (text: string): DICT =>
   compose(
     map(parse),
--- src/exams/01/store.ts
+++ src/exams/01/store.ts
@@ -8,6 +8,8 @@ const stringify = (dict: DICT): string =>
     toPairs
   )(dict);
 
+// Complexity: O(n x m), which is equal to the complexity to traverse a 2d array
+// where n is size of array & m is size of dictionary
 export default (dicts: DICT[]): string =>
   compose(
     join('\n'),
